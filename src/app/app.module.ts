25b7eac42ad53f87bb48eae1175a316768041785
Morgan Scofield <mkscofie@gmail.com>
2019-11-12 19:05:56 -0800

    Updating dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasklistComponent } from './tasklist/tasklist.component';
@@ -23,9 +20,6 @@ import { TasklistComponent } from './tasklist/tasklist.component';
     ReactiveFormsModule,
     NgbModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
   ],
   providers: [],
   bootstrap: [AppComponent]
